edfc431666b5c5737c800bbeab0ca700c73a6e6c
shakirr45 <shakirr45@gmail.com>
2025-05-31 01:04:29 +0600

    first commit

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -48,10 +48,6 @@
                                             </button>
                                         </form>
                                         @endcan
-                                        <button type="button" class="btn btn-success swalDefaultSuccess">
-                                            Launch Success Toast
-                                        </button>
-
 
                                     </form>
                                 </td>
