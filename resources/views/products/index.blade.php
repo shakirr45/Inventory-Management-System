3c320051a35a45373a244d92f2caa4da3e65fc3e
shakirr45 <shakirr45@gmail.com>
2025-05-31 00:48:18 +0600

    first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,67 @@
+@extends('admin.layouts.app')
+
+@section('content')
+<div class="container-fluid mt-3">
+    <div class="row">
+        <div class="col-12">
+
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Product List</h3>
+                    @can('product-create')
+                    <a href="{{ route('products.create') }}" class="btn btn-success float-right">+ Create New Product</a>
+                    @endcan
+                </div>
+                <div class="card-body table-responsive">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>Name</th>
+                                <th>Details</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $product)
+                            <tr>
+                                <td>{{ ++$i }}</td>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->detail }}</td>
+                                <td>
+                                    <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+                                        <a href="{{ route('products.show', $product->id) }}" class="btn btn-sm btn-outline-danger" title="Show Product">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                        @can('product-edit')
+                                        <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-outline-primary">
+                                            <i class="fas fa-edit"></i> Edit
+                                        </a>
+                                        @endcan
+
+                                        @can('product-delete')
+                                        <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure you want to delete this product?')">
+                                                <i class="fas fa-trash-alt"></i> Delete
+                                            </button>
+                                        </form>
+                                        @endcan
+
+
+
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
